9e17af96e33cde0bc94c4ac025af8e9dd53d425f
Ahmed Hisham <37244966+ahmedhisham73@users.noreply.github.com>
2019-04-15 16:29:04 +0200

    Add files via upload

---
+++ jira.py
@@ -0,0 +1,42 @@
+from .chatterbot import ChatBot
+
+bot = ChatBot('Norman')
+
+bot = ChatBot(
+    'Norman',
+    storage_adapter='chatterbot.storage.SQLStorageAdapter',
+    database_uri='sqlite:///database.sqlite3'
+)
+
+bot = ChatBot(
+    'Norman',
+    storage_adapter='chatterbot.storage.SQLStorageAdapter',
+    logic_adapters=[
+        'chatterbot.logic.MathematicalEvaluation',
+        'chatterbot.logic.TimeLogicAdapter'
+    ],
+    database_uri='sqlite:///database.sqlite3'
+)
+while True:
+    try:
+        bot_input = bot.get_response(input())
+        print(bot_input)
+
+    except(KeyboardInterrupt, EOFError, SystemExit):
+        break
+from chatterbot.trainers import ListTrainer
+
+trainer = ListTrainer(bot)
+
+trainer.train([
+    'How are you?',
+    'I am good.',
+    'That is good to hear.',
+    'Thank you',
+    'You are welcome.',
+])
+
+
+
+
+
